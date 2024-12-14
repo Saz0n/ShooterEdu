275e18f708dbc1a4b48691d4cfd498656a03a800
Eduardo Sazan <69261696+Saz0n@users.noreply.github.com>
2024-12-14 12:20:12 -0300

    Add files via upload

---
+++ code/Enemy.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from code.Const import ENTITY_SPEED, ENTITY_SHOT_DELAY
+from code.EnemyShot import EnemyShot
+from code.Entity import Entity
+
+
+class Enemy(Entity):
+    def __init__(self, name: str, position: tuple):
+        super().__init__(name, position)
+        self.shot_delay = ENTITY_SHOT_DELAY[self.name]
+        self.vertical_direction = 1
+        self.vertical_speed = 2
+
+    def move(self):
+        self.rect.centerx -= ENTITY_SPEED[self.name]
+        if self.name == "Enemy3":
+            self.rect.centery += self.vertical_speed * self.vertical_direction
+            if self.rect.bottom >= 324:
+                self.rect.bottom = 324
+                self.vertical_direction = -1
+                self.vertical_speed = 2
+            elif self.rect.top <= 0:
+                self.rect.top = 0
+                self.vertical_direction = 1
+                self.vertical_speed *= 2
+
+    def shoot(self):
+        self.shot_delay -= 1
+
+        if self.shot_delay == 0:
+            self.shot_delay = ENTITY_SHOT_DELAY[self.name]
+            return EnemyShot(name=f'{self.name}Shot', position=(self.rect.centerx, self.rect.centery))
